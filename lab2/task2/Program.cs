52686360f4195f3e247a8fef7b77fbccaf7b73a9
matzhananar <anr.matzhn@gmail.com>
2019-02-15 13:01:46 +0600

    its ok

--- lab2/task2/Program.cs
+++ lab2/task2/Program.cs
@@ -26,13 +26,13 @@ namespace task2
         }
        static void Main(string[] args)
         {
-            StreamReader s = new StreamReader(@"C:\Users\Анар\Documents\pp\hello\hello.txt");
-            StreamWriter st = new StreamWriter(@"C:\Users\Анар\Documents\pp\hello\bye.txt");
-            string[] t = s.ReadToEnd().Split();
+            StreamReader s = new StreamReader(@"C:\Users\Анар\Desktop\lab2\task2\hello.txt");   //Стримридер для чтения файла
+            StreamWriter st = new StreamWriter(@"C:\Users\Анар\Desktop\lab2\task2\bye.txt");    //Стримрайтер для записи в текстовой файл
+            string[] t = s.ReadToEnd().Split();                                                 //читает до конца каждого элемента раздельно 
             int[] a = new int[t.Length];
             for(int i=0; i<t.Length; i++)
             {
-                a[i] = int.Parse(t[i]);
+                a[i] = int.Parse(t[i]);                                                         //string like int
             }
             for(int i=0; i<t.Length; i++)
             {
